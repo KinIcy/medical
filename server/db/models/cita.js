31a0b76080f5d43434638a6abb2a745edca8e794
KinIcy NET <nkinicy@gmail.com>
2018-04-21 18:06:45 -0500

    updated db models

--- server/db/models/cita.js
+++ server/db/models/cita.js
@@ -2,13 +2,19 @@ import Sequelize from 'sequelize';
 
 export default function (db) {
   const Cita = db.define('cita', {
+    idCita: {
+      type: Sequelize.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     fecha: { type: Sequelize.DATEONLY, allowNull: false },
     hora: { type: Sequelize.TIME, allowNull: false },
     duracion: { type: Sequelize.INTEGER, allowNull: false },
     comentario: { type: Sequelize.TEXT },
     formulacion: { type: Sequelize.TEXT },
     observaciones: { type: Sequelize.TEXT },
-    estado: { type: Sequelize.ENUM('pendiente', 'cancelada', 'atendida'), allowNull: false, defaultValue: 'pendiente' },
+    estado: { type: Sequelize.ENUM('reservada', 'cancelada', 'atendida', 'disponible'), allowNull: false, defaultValue: 'reservada' },
   }, {
     classMethods: {
       associate({ Medico, Paciente }) {
--- server/db/models/horario.js
+++ server/db/models/horario.js
@@ -2,7 +2,13 @@ import Sequelize from 'sequelize';
 
 export default function (db) {
   const Horario = db.define('horario', {
-    fecha: { type: Sequelize.DATEONLY, allowNull: false },
+    idHorario: {
+      type: Sequelize.TINYINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dia: { type: Sequelize.ENUM('L', 'M', 'X', 'J', 'V', 'S', 'D'), allowNull: false },
     horaInicio: { type: Sequelize.TIME, allowNull: false },
     horaFin: { type: Sequelize.TIME, allowNull: false },
   }, {
