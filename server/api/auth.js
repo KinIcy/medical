6d95bccc7aaa9346694d5ededb90d4f5e422312e
valentin11 <valentinfer11@gmail.com>
2018-04-21 14:48:56 -0500

    Prueba

--- server/api/auth.js
+++ server/api/auth.js
@@ -6,8 +6,8 @@ const secret = 'dummy' || process.env.SECRET;
 const router = Router();
 
 router.post('/login', async (req, res) => {
+  res.status(400).send({ error: 'En contruccion'});
   const { tipo, usuario, contrasena } = req.body;
-  res.status(400).send({ error: 'Nombre de usuario invalido'});
   let accessToken;
   if (!usuario || usuario.length < 3) {
     res.status(400).send({ error: 'Nombre de usuario invalido' });
