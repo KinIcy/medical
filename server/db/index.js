cc7a72b5b78338aebfbea48285bd323435bf83c0
valentin11 <valentinfer11@gmail.com>
2018-05-04 13:21:34 -0500

    Se crean pacientes y medicos de pruebas

--- server/db/index.js
+++ server/db/index.js
@@ -94,8 +94,92 @@ export async function initialize() {
       idMedico: medico.dataValues.idMedico,
     },
   });
+
+
+ // Se crean datos de prueba...
+ createPatient("11111111", "PP", "María Laura", "Diaz Lunas", "9999999999","activo",
+ "5555555555", "mdiaz@gmail.com", "Cali")
+
+ createPatient("22222222", "PP", "Henry", " Laus Hyins", "9999999999","activo",
+ "5555555555", "hlaus@gmail.com", "Cali")
+
+ createPatient("33333333", "PP", "Florencia", "Frachia", "9999999999","activo",
+ "5555555555", "ffrachia@gmail.com", "Cali")
+
+ createMedical("DIEGO.FUENTES", "Diego", "Fuentez", "Cardiologo","999999999", "activo")
+ createMedical("JUAN.LUZ", "Juan", "Luz", "Cardiologo","999999999", "activo")
+ createMedical("DIANA.RIOS", "Diana María", "Rios", "Neurologo","999999999", "activo")
+ createMedical("JOSE.BENAVIDEZ", "Jose", "Benaviez", "General","999999999", "activo")
+ createMedical("PALOMA.SAMBUSSI", "Paloma", "Sambussi", "General","999999999", "activo")
+ createMedical("LISA.DELBOSQUE", "Lisa", "Del Bosque", "General","999999999", "activo")
+ createMedical("ARIEL.PEREZ", "Ariel", "Perez", "General","999999999", "activo")
+ createMedical("ALVARO.SUSQUI", "Álvaro", "Susqui", "General","999999999", "activo")
+ createMedical("LILY.ASQUI", "Lily", "Asqui", "General","999999999", "activo")
+ createMedical("TOM.SUIEZ", "Tom", "Suiez", "General","999999999", "activo")
+ createMedical("URBELO.NANO", "Urbelo", "Nano", "General","999999999", "activo")
+ createMedical("SUAD.TELLO", "Suad", "Tello", "General","999999999", "activo")
+ createMedical("HENRY.DIAZ", "Hendry", "Dias", "General","999999999", "activo")
+ createMedical("TOM.SUIEZ", "Tom", "Suiez", "General","999999999", "activo")
+ createMedical("ANA.REY", "Ana María", "Rey", "Cardiologo","999999999", "activo")
 }
 
 export default {
   db, initialize, models,
 };
+
+function createPatient(pnumId, ptipoId, pnombres, papellidos, pcontrasena, pestado
+                    ptelefono, pcorreo, pciudad){
+  await db.authenticate();
+  await db.sync();
+  const [paciente] = await Paciente.findOrCreate({
+    where: { numId: pnumId, tipoId: ptipoId },
+    defaults: {
+      tipoId: ptipoId,
+      numId: pnumId,
+      nombres: pnombres,
+      apellidos: papellidos,
+      contrasena: pcontrasena,
+      fechaNacimiento: new Date(2000, 1, 1),
+      estado: pestado,
+      telefono: ptelefono,
+      correo: pcorreo,
+      ciudad: pciudad,
+    },
+  });
+}
+
+function createMedical(puser, pnombres, papellidos, pespecialidad, pcontrasena pestado){
+  await db.authenticate();
+  await db.sync();
+  const [medico] = await Medico.findOrCreate({
+    where: { usuario: puser },
+    defaults: {
+      nombres: pnombres,
+      apellidos: papellidos,
+      usuario: puser,
+      especialidad: pespecialidad,
+      contrasena: pcontrasena,
+      estado: pestado,
+      esAdmin: true,
+    },
+  });
+}
+
+function createCita(pfecha, phora, pduracion, pcomentario, pestado, pidMedico, pidPaciente){
+  await db.authenticate();
+  await db.sync();
+  await Cita.findOrCreate({
+    where: { comentario: pcomentario },
+    defaults: {
+      fecha: pfecha,
+      hora: phora,
+      duracion: pduracion,
+      comentario: pcomentario,
+      formulacion: '',
+      observaciones: '',
+      estado: pestado,
+      idMedico: pidMedico,
+      idPaciente: pidPaciente,
+    },
+  });
+}
