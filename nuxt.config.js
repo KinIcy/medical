a7e6a2c4aeef32edc898210110b862bb526dcf8c
KinIcy NET <nkinicy@gmail.com>
2018-05-10 21:23:05 -0500

    updated axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,11 +54,15 @@ module.exports = {
     strategies: {
       local: {
         endpoints: {
-          login: { url: '/api/auth/login', method: 'post', propertyName: 'token' },
-          logout: { url: '/api/auth/logout', method: 'post' },
-          user: { url: '/api/auth/user', method: 'get', propertyName: 'user' },
+          login: { url: 'auth/login', method: 'post', propertyName: 'token' },
+          logout: { url: 'auth/logout', method: 'post' },
+          user: { url: 'auth/user', method: 'get', propertyName: 'user' },
         },
       },
     },
+  },
+  // Axios global configuration
+  axios: {
+    baseURL: process.env.API_URL || 'http://localhost:3000/api/',
   },
 };
